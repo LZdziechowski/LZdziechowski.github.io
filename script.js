6312c6afc86138334fe45a83f335f29a41f90d28
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-01-25 22:43:45 +0100

    Correction of wrong URL path

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = '  https://serene-eyrie-65608.herokuapp.com/v1/task/';
-   const trelloApiRoot = '  https://serene-eyrie-65608.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://serene-eyrie-65608.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://serene-eyrie-65608.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
